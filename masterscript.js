afbcab74a5a216e66643b05902ac8ee21eccac21
Shubhdeep Sarkar <30292255+SarkarShubhdeep@users.noreply.github.com>
2019-11-18 18:00:02 +0530

    Add files via upload

---
+++ masterscript.js
@@ -0,0 +1,18 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    center: true,
+    autoWidth:true,
+    autoplay:true,
+    autoplayTimeout:2500,
+    autoplayHoverPause:true,
+    autoplaySpeed: 700,
+
+    // // nav:true,
+    // responsive:{
+
+    //     1000:{
+    //         items:4
+    //     }
+    // }
+})
